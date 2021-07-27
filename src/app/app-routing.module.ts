8d179e38764c8c08faf1680467573ae33515ab0f
Erin452 <erinchen452@gmail.com>
2021-07-27 23:55:56 +0800

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
+import { PageComponent } from './page/page.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/app', pathMatch: 'full' }, // 有使用 redirectTo 的話，要加入 pathMatch 設定，預設是 prefix
+  { path: 'twoway', component: TwoWayBindingComponent },
+  { path: 'page', component: PageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageComponent } from './page/page.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageComponent,
+    TwoWayBindingComponent
   ],
   imports: [
     BrowserModule,

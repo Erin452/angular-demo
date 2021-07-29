46ee83ac3000cd8cf929139d51bddc23318a9971
Erin452 <erinchen452@gmail.com>
2021-07-30 04:32:29 +0800

    add html event example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
 import { ButtonComponent } from './button/button.component';
+import { HtmlEventComponent } from './html-event/html-event.component';
 import { PageComponent } from './page/page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' }, // 有使用 redirectTo 的話，要加入 pathMatch 設定，預設是 prefix
   { path: 'twoway', component: TwoWayBindingComponent },
   { path: 'btn', component: ButtonComponent },
+  { path: 'event', component: HtmlEventComponent },
   { path: 'page', component: PageComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
 import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
 import { ButtonComponent } from './button/button.component';
+import { HtmlEventComponent } from './html-event/html-event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageComponent,
     TwoWayBindingComponent,
-    ButtonComponent
+    ButtonComponent,
+    HtmlEventComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSlideToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
